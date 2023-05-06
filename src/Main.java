8361561863e2f3f18cc102d81b4606a610b3b40f
DNS <glotov-aleks@mail.ru>
2023-05-07 01:25:39 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
-        System.out.println("Hello world!");
+    public static void task1() {
+        System.out.println("Решение задачи №1");
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        System.out.println(Arrays.toString(a));
+        double[] s = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(s));
+        byte[] c = new byte[]{1, 2, 4, 5, 6};
+        System.out.println(Arrays.toString(c));
     }
 }
