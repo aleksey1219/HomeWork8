18195c31bff37288af9c2a1c45cf2f8c24785cac
DNS <glotov-aleks@mail.ru>
2023-05-07 02:07:07 +0500

    task3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -43,6 +44,37 @@ public class Main {
         }
         for (int i = 0; i < c.length; i++) {
             if (i == c.length - 1) {
+                System.out.println(c[i]);
+                break;
+            }
+            System.out.print(c[i] + ",");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Решение задачи №3");
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        double[] s = {1.57, 7.654, 9.986};
+        byte[] c = new byte[]{1, 2, 4, 5, 6};
+        for (int i = a.length - 1; i >= 0; i--) {
+            if (i == a.length - a.length) {
+                System.out.println(a[i]);
+                break;
+            }
+            System.out.print(a[i] + ",");
+        }
+        for (int i = s.length - 1; i >= 0; i--) {
+            if (i == s.length - s.length) {
+                System.out.println(s[i]);
+                break;
+            }
+            System.out.print(s[i] + ",");
+        }
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (i == c.length - c.length) {
                 System.out.println(c[i]);
                 break;
             }
